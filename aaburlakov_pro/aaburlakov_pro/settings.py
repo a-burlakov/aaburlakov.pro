30b0af345df85c01d043d83975fc33be2b17e50b
a-burlakov <fln@mail.ru>
2023-02-03 12:07:46 +0500

    learn: first lesson about general Django things.

--- aaburlakov_pro/aaburlakov_pro/settings.py
+++ aaburlakov_pro/aaburlakov_pro/settings.py
@@ -10,6 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+# Основная логика работы Django может быть описана так:
+# - клиент делает запрос к нам на сервер
+# - запрос попадает на Web-сервер (Apache, Nginx...)
+# - далее Web-сервер через интерфейс wsgi (есть еще асинхронный asgi) передает
+# этот запрос в фреймворк Django
+# - сам Django, исходя из представлений (Views) конкретного своего приложения
+# формирует HTML и возвращает ответ в Web-сервер, а тот - клиенту
+# Это, конечно, все очень упрощенно.
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- aaburlakov_pro/aaburlakov_pro/wsgi.py
+++ aaburlakov_pro/aaburlakov_pro/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+# WSGI - это Web Server Gateway Interface. Это стандарт взаимодействия между
+# программой на Python (Django в нашем случае) и веб-сервером (Apache, Nginx...)
+
 import os
 
 from django.core.wsgi import get_wsgi_application
