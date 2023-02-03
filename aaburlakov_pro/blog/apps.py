bbbb52412548f056ed4742f88005c102c7f95463
a-burlakov <fln@mail.ru>
2023-02-03 12:12:55 +0500

    feat: "blog" app

--- aaburlakov_pro/blog/apps.py
+++ aaburlakov_pro/blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PersonalSiteConfig(AppConfig):
+class BlogConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "personal_site"
+    name = "blog"
