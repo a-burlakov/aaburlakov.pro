9ca1c5593c03b3d5a0dfaa836af846be6681e22d
a-burlakov <fln@mail.ru>
2023-03-03 17:32:19 +0500

    feat: some construction to make STATIC for with DEBUG=False.

--- aaburlakov_pro/urls.py
+++ aaburlakov_pro/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.template.defaulttags import url
 from django.urls import path, include
+from django.views.static import serve
 
 from aaburlakov_pro import settings
 
@@ -18,3 +20,17 @@ if settings.DEBUG:
     ] + urlpatterns
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+else:
+    urlpatterns += [
+        url(
+            f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+            serve,
+            {"document_root": settings.MEDIA_ROOT},
+        ),
+        url(
+            f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+            serve,
+            {"document_root": settings.STATIC_ROOT},
+        ),
+    ]
