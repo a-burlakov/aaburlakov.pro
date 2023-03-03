47698a1fccdc6c112611c58701e9be7a322808d8
a-burlakov <fln@mail.ru>
2023-03-03 21:44:31 +0500

    feat: reduced cache time to 0 seconds

--- apps/personal_site/urls.py
+++ apps/personal_site/urls.py
@@ -4,10 +4,10 @@ from django.views.decorators.cache import cache_page
 from .views import *
 
 urlpatterns = [
-    path("", cache_page(60 * 1)(aaburlakov), name="home"),
+    path("", (aaburlakov), name="home"),
     path(
         "blog/<slug:slug>/",
-        cache_page(60 * 1)(ArticleDetail.as_view()),
+        (ArticleDetail.as_view()),
         name="article_detail",
     ),
     path("api/v1/recentarticles/", RecentArticlesAPIView.as_view()),
