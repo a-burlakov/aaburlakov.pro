3519f32fb24cb2dd7d1624275ff06540ec6b64de
a-burlakov <fln@mail.ru>
2023-03-03 21:17:52 +0500

    refactor: returned cache.

--- apps/personal_site/urls.py
+++ apps/personal_site/urls.py
@@ -4,13 +4,10 @@ from django.views.decorators.cache import cache_page
 from .views import *
 
 urlpatterns = [
-    # TODO: вернуть кэш
-    # path("", cache_page(60 * 15)(aaburlakov), name="home"),
-    path("", (aaburlakov)),
+    path("", cache_page(60 * 15)(aaburlakov), name="home"),
     path(
         "blog/<slug:slug>/",
-        # cache_page(60 * 15)(ArticleDetail.as_view()),
-        (ArticleDetail.as_view()),
+        cache_page(60 * 15)(ArticleDetail.as_view()),
         name="article_detail",
     ),
     path("api/v1/recentarticles/", RecentArticlesAPIView.as_view()),
