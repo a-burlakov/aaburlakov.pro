8e3cbfe132abb7a752dd276f9de3895e3e1b5fa7
a-burlakov <fln@mail.ru>
2023-03-09 16:58:11 +0500

    refactor: trying to deploy that damn project

--- aaburlakov_pro/settings.py
+++ aaburlakov_pro/settings.py
@@ -158,7 +158,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATICFILES_DIRS = []
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # STATICFILES_FINDERS = ("django.contrib.staticfiles.finders.FileSystemFinder",)
 
